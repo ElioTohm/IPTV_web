fa3c769072d1f7fbf14fcdf0e637a67603f10243
elio_tohme <eliotohme@gmail.com>
2017-08-10 16:29:08 +0300

    channel  table update

--- IPTV/database/migrations/2017_07_21_103109_create_channels_table.php
+++ IPTV/database/migrations/2017_07_21_103109_create_channels_table.php
@@ -16,6 +16,7 @@ class CreateChannelsTable extends Migration
         Schema::create('channels', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('thumbnail');
             $table->string('description');
             $table->timestamps();
         });
