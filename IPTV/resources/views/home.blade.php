33a61dd1c7e9ce35e3163719289dbcc6d81832dd
elio_tohme <eliotohme@gmail.com>
2017-08-10 18:57:37 +0300

    RE-added the notification for demoing only

--- IPTV/resources/views/home.blade.php
+++ IPTV/resources/views/home.blade.php
@@ -10,7 +10,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Notification and Client Settings</div>
                 <div class="panel-body">
-                    test
+                    <a href="/clientnotification/0">send notification to all clients</a>
                 </div>
             </div>
         </div>
