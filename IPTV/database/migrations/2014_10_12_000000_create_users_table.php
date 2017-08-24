8e8409bbe019ffaa32a9aa05b3bb9facf45c06db
elio_tohme <eliotohme@gmail.com>
2017-08-24 14:31:12 +0300

    removed roles since i can add clients from artisan

--- IPTV/database/migrations/2014_10_12_000000_create_users_table.php
+++ IPTV/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,29 +23,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        
-        // create role table for users 
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
-
-        Schema::create('user_roles', function (Blueprint $table) {
-            $table->integer('user')->unsigned();
-            $table->integer('role')->unsigned();
-            $table->primary(['user', 'role']);
-            $table->foreign('user')
-                  ->references('id')->on('users')
-                  ->onDelete('cascade')
-                  ->onUpdate('cascade');
-            $table->foreign('role')
-                  ->references('id')->on('roles')
-                  ->onDelete('cascade')
-                  ->onUpdate('cascade');
-            $table->timestamps();
-        });
-
+       
     }
 
     /**
