d2b6ad4e32b07922a84c394c974cf497414736dd
elio_tohme <eliotohme@gmail.com>
2017-09-16 15:03:05 +0300

    shotening client registration tkn done

--- IPTV/app/Http/Controllers/ApiController.php
+++ IPTV/app/Http/Controllers/ApiController.php
@@ -7,9 +7,45 @@ use App\Channel;
 use App\Genre;
 use App\Client;
 use App\Device;
+use App\User;
+use App\oAuthClient;
+use GuzzleHttp\Client as GuzzleClient;
 
 class ApiController extends Controller
 {
+    /** 
+     * Function making it easier on the client to register device
+     * Code will take the ID oc authclient and first 4 character of the secret
+     * The function will check if there is a secret for the client starting 
+     * with the first 4 character sent
+     * The function will make an auth request to passport or return 401 respectively 
+     */
+    public function register($id, $sentsecret) {
+
+        $oauthclient = oAuthClient::where('id', $id)
+                                    ->where('revoked', 0)
+                                    ->first();
+        if ($oauthclient != null){
+            $secret = $oauthclient->secret;
+            $result = substr($secret, 0, 4);
+
+            if (strcmp($result, $sentsecret) == 0 ){
+
+                $user = User::find(1);
+                $token = $user->createToken($oauthclient->name);
+
+                return response()->json([
+                    'token_type' => 'Bearer',
+                    'expires_in' => $token->token->expires_at,
+                    'access_token' => $token->accessToken
+                ]);
+            }
+        }
+        return 401;
+
+        
+    }
+
     // Get Channels
     public function getChannel (Request $request) 
     {
