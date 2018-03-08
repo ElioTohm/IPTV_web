729051c03b6ab2b61f9f85ecd234df9894fe5ba9
elio <eliotohme@gmail.com>
2018-03-08 17:02:27 +0200

    added section item migration model and Controller

---
+++ IPTV/database/migrations/2018_03_08_140531_create_section_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('section_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('section')->unsigned();
+            $table->foreign('section')
+                  ->references('id')->on('sections')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->string('name');
+            $table->string('poster');
+            $table->mediumText('description')->nullable();
+            $table->boolean('reservation')->default(False);
+            $table->double('longitude', 10, 8)->nullable();
+            $table->double('latitude', 10, 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('section_items');
+    }
+}
