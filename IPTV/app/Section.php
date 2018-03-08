d8f7e9dc1bef998359b1b4aaf42b5279a65f0043
elio <eliotohme@gmail.com>
2018-03-08 15:02:51 +0200

    added section migration model and resource controller

---
+++ IPTV/app/Section.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Section extends Model
+{
+    use Searchable;
+
+    public $asYouType = false;
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        return $array;
+    }
+
+    public function getIconAttribute($value)
+    {
+        return $url = Storage::disk('public')->url('/hotel/images/' . $value);
+    }
+
+}
