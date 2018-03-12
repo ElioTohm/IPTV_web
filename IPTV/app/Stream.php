5d6ba2b1964af45520a9ffa8d3edd736e4f69b4d
elio <eliotohme@gmail.com>
2018-03-12 10:30:10 +0200

    stream attribute changed

--- IPTV/app/Stream.php
+++ IPTV/app/Stream.php
@@ -2,13 +2,31 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Stream extends Model
 {
-    //
+    
+    use Searchable;
+
+    public $asYouType = true;
+
     protected $table = 'streams';
     protected $hidden = array('created_at', 'updated_at');
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        return $array;
+    }
 
     public function type ()
     {
@@ -21,5 +39,15 @@ class Stream extends Model
 
     public function movies () {
         return $this->hasOne('App\Movie', 'id', 'movie');
+    }
+
+    public function getVidStreamAttribute($value)
+    {
+        if ($this->channel == NULL) {
+            return Storage::disk('public')->url('movies/' . $value);
+        } else {
+            return $value;
+        }
+        
     }
 }
