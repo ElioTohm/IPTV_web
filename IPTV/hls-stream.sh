7568ae059d39ad122a94a22349ab3e46272a01f8
elio <eliotohme@gmail.com>
2018-03-27 14:22:14 +0300

    created bash file for ffmpeg

---
+++ IPTV/hls-stream.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Usage create-vod-hls.sh SOURCE_FILE [OUTPUT_NAME]
+[[ ! "${1}" ]] && echo "Usage: create-vod-hls.sh SOURCE_FILE [OUTPUT_NAME]" && exit 1
+
+
+source="${1}"
+target="${2}"
+
+#########################################################################
+
+segment_target_duration=10
+segment_list_size=3
+parent_dir="./storage/app/public/streams"
+#########################################################################
+
+if [[ ! "${target}" ]]; then
+  target="${source##*/}" # leave only last component of path
+fi
+mkdir -p ${target}
+
+# static parameters that are similar for all renditions
+static_params+=" -hls_time $segment_target_duration"
+static_params+=" -hls_list_size $segment_list_size"
+static_params+=" -hls_flags delete_segments"
+
+# misc params
+misc_params="-re -hide_banner"
+cmd+=" -c:a copy -c:v copy ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}_%03d.ts ${parent_dir}/${target}/${target}.m3u8"
+
+# start conversion
+echo "ffmpeg ${misc_params} -i ${source} ${cmd}"
+
+ffmpeg ${misc_params} -i ${source} ${cmd}
