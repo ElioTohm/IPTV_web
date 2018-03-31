13afce4507e6bba0eaac99f201301a811e12ed89
elio <eliotohme@gmail.com>
2018-03-31 11:34:09 +0300

    test.sh update

--- IPTV/test.sh
+++ IPTV/test.sh
@@ -28,25 +28,30 @@ static_params+=" -hls_flags delete_segments"
 # misc params
 misc_params=" -re -hide_banner"
 
-master_playlist+="#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"bipbop_audio\",LANGUAGE=\"eng\",NAME=\"BipBop Audio 2\",AUTOSELECT=YES,DEFAULT=YES,URI=\"alternate_audio_aac/prog_index.m3u8\""
-master_playlist+="#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID=\"subs\",NAME=\"English\",DEFAULT=YES,AUTOSELECT=YES,FORCED=NO,LANGUAGE=\"en\",URI=\"subtitles/eng/prog_index.m3u8\""
+master_playlist+="#EXTM3U\n"
+master_playlist+="#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"bipbop_audio\",LANGUAGE=\"eng\",NAME=\"English\",AUTOSELECT=YES,DEFAULT=YES,URI=\"A.m3u8\"\n"
+master_playlist+="#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID=\"subs\",NAME=\"English\",DEFAULT=YES,AUTOSELECT=YES,FORCED=NO,LANGUAGE=\"en\",URI=\"S.m3u8\"\n"
+master_playlist+="#EXT-X-STREAM-INF:BANDWIDTH=263851,RESOLUTION=416x234,AUDIO=\"bipbop_audio\",SUBTITLES=\"subs\"\n"
+master_playlist+="V.m3u8\n"
 
-cmd+=" -codec copy -map 0 ${static_params}"
-cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}_%03d.ts ${parent_dir}/${target}/master.m3u8"
+cmd+=" -codec copy -map 0:v:? ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}V_%03d.ts ${parent_dir}/${target}/V.m3u8"
+cmd+=" -codec copy -map 0:a:? ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}A%v_%03d.ts ${parent_dir}/${target}/A.m3u8"
+cmd+=" -codec copy -map 0:s:? ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/S_%03d.vtt ${parent_dir}/${target}/S.m3u8"
+
+# create master playlist file
+echo -e "${master_playlist}" > ${parent_dir}/${target}/master.m3u8
 
 # start conversion
 echo -e "Executing command:\nffmpeg ${misc_params} -i ${source} ${cmd}"
 ffmpeg ${misc_params} -i ${source} ${cmd}
 
-# create master playlist file
-echo -e "${master_playlist}" > ${target}/playlist.m3u8
-
-#EXT-X-STREAM-INF:BANDWIDTH=263851,CODECS="mp4a.40.2, avc1.4d400d",RESOLUTION=416x234,AUDIO="bipbop_audio",SUBTITLES="subs"
-gear1/prog_index.m3u8
 
 # ffmpeg ${misc_params} -i ${source} -codec: copy \
-#   -map 0 -f hls \
-#   -var_stream_map "a:0,agroup:aud v:0,s:0,agroup:aud" \
+#   -map 0:a -map 0:v -f hls \
+#   -var_stream_map "a:0,agroup:aud v:0,agroup:aud" \
 #   -hls_time 10 \
 #   -hls_list_size 3 \
 #   -hls_flags delete_segments \
