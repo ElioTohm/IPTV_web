cfd59bacbc2840e8f5599236cacd96011a0d0784
elio <eliotohme@gmail.com>
2018-04-04 14:26:48 +0300

    updates test executiong file for ffmpeg

--- IPTV/test.sh
+++ IPTV/test.sh
@@ -29,31 +29,21 @@ static_params+=" -hls_flags delete_segments"
 misc_params=" -re -hide_banner"
 
 master_playlist+="#EXTM3U\n"
-master_playlist+="#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"bipbop_audio\",LANGUAGE=\"eng\",NAME=\"English\",AUTOSELECT=YES,DEFAULT=YES,URI=\"A.m3u8\"\n"
-master_playlist+="#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID=\"subs\",NAME=\"English\",DEFAULT=YES,AUTOSELECT=YES,FORCED=NO,LANGUAGE=\"en\",URI=\"S.m3u8\"\n"
-master_playlist+="#EXT-X-STREAM-INF:BANDWIDTH=263851,RESOLUTION=416x234,AUDIO=\"bipbop_audio\",SUBTITLES=\"subs\"\n"
+master_playlist+="#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"audio\",LANGUAGE=\"ost\",NAME=\"Arabic\",AUTOSELECT=YES,DEFAULT=YES,URI=\"A1.m3u8\"\n"
+master_playlist+="#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"audio\",LANGUAGE=\"eng\",NAME=\"English\",AUTOSELECT=YES,DEFAULT=YES,URI=\"A2.m3u8\"\n"
+master_playlist+="#EXT-X-STREAM-INF:BANDWIDTH=263851,RESOLUTION=416x234,AUDIO=\"audio\"\n"
 master_playlist+="V.m3u8\n"
 
-cmd+=" -codec copy -map 0:v:? ${static_params}"
-cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}V_%03d.ts ${parent_dir}/${target}/V.m3u8"
-cmd+=" -codec copy -map 0:a:? ${static_params}"
-cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}A%v_%03d.ts ${parent_dir}/${target}/A.m3u8"
-cmd+=" -codec copy -map 0:s:? ${static_params}"
-cmd+=" -hls_segment_filename ${parent_dir}/${target}/S_%03d.vtt ${parent_dir}/${target}/S.m3u8"
+cmd+=" -codec copy -map 0:0 ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/V_%03d.ts ${parent_dir}/${target}/V.m3u8"
+cmd+=" -codec copy -map 0:1 ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/A1%v_%03d.ts ${parent_dir}/${target}/A1.m3u8"
+cmd+=" -codec copy -map 0:2 ${static_params}"
+cmd+=" -hls_segment_filename ${parent_dir}/${target}/A2%v_%03d.ts ${parent_dir}/${target}/A2.m3u8"
 
-# create master playlist file
+#create master playlist file
 echo -e "${master_playlist}" > ${parent_dir}/${target}/master.m3u8
 
 # start conversion
 echo -e "Executing command:\nffmpeg ${misc_params} -i ${source} ${cmd}"
 ffmpeg ${misc_params} -i ${source} ${cmd}
-
-
-# ffmpeg ${misc_params} -i ${source} -codec: copy \
-#   -map 0:a -map 0:v -f hls \
-#   -var_stream_map "a:0,agroup:aud v:0,agroup:aud" \
-#   -hls_time 10 \
-#   -hls_list_size 3 \
-#   -hls_flags delete_segments \
-#   -master_pl_name master.m3u8 \
-#   ${parent_dir}/${target}/${target}_%v.m3u8
