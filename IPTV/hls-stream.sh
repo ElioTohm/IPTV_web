d5569580bbaeae5ce79fbd22e496400b2465c7d1
elio_tohme <eliotohme@gmail.com>
2018-05-10 12:52:39 +0300

    update ffmpeg used as sudo

--- IPTV/hls-stream.sh
+++ IPTV/hls-stream.sh
@@ -34,4 +34,4 @@ cmd+=" -hls_segment_filename ${parent_dir}/${target}/${target}_%03d.ts ${parent_
 # start conversion
 echo "ffmpeg ${misc_params} -i ${source} ${cmd}"
 
-ffmpeg ${misc_params} -i ${source} ${cmd} </dev/null >/dev/null 2>/home/user/logs/ffmpeg-${target}.log &
+/home/user/bin/ffmpeg ${misc_params} -i ${source} ${cmd} </dev/null >/dev/null 2>/var/log/ffmpeg-${target}.log &
