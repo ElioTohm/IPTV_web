21ed40d153ef1ddbbe66955cf0c434022ea97aeb
elio_tohme <eliotohme@gmail.com>
2018-05-17 18:15:12 +0300

    test

--- IPTV/app/Jobs/CatchUp.php
+++ IPTV/app/Jobs/CatchUp.php
@@ -39,8 +39,9 @@ class CatchUp implements ShouldQueue
         $stream = Stream::find($this->STREAM_ID);
         $logpath = env('HOME_ENV_PATH') .'/storage/log/worker.log';
         $exec_file = env('HOME_ENV_PATH') . 'hls-stream.sh';
-        $this->process = new Process('bash ' . $exec_file . ' ' . $stream->vid_stream .  '?fifo_size=1000000 ' . $stream->id . ' '. intdiv(env('DURATION_CATCHUP'), 60). ' '. env('HOME_ENV_PATH').'/storage/app/public/store/streams');
-        $this->process->run();
+        shell_exec('bash ' . $exec_file . ' ' . $stream->vid_stream .  '?fifo_size=1000000 ' . $stream->id . ' '. intdiv(env('DURATION_CATCHUP'), 60). ' '. env('HOME_ENV_PATH').'/storage/app/public/store/streams');
+        // $this->process = new Process('bash ' . $exec_file . ' ' . $stream->vid_stream .  '?fifo_size=1000000 ' . $stream->id . ' '. intdiv(env('DURATION_CATCHUP'), 60). ' '. env('HOME_ENV_PATH').'/storage/app/public/store/streams');
+        // $this->process->run();
     }
 
 }
