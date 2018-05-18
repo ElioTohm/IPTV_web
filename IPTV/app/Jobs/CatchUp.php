54d36c7e336da6222913b7bcbefa3a8babb881f1
elio_tohme <eliotohme@gmail.com>
2018-05-18 18:04:21 +0300

    LORD OF THE FALLEN HELP

--- IPTV/app/Jobs/CatchUp.php
+++ IPTV/app/Jobs/CatchUp.php
@@ -44,10 +44,10 @@ class CatchUp implements ShouldQueue
         // $executable = 'bash ' . $exec_file . ' ' . $stream->vid_stream .  '?fifo_size=1000000 ' . $stream->id . ' '. intdiv(env('DURATION_CATCHUP'), 60). ' '. env('HOME_ENV_PATH').'/storage/app/public/store/streams';
         $path = env('HOME_ENV_PATH').'/storage/app/public/store/streams';
         $cmd = " -codec copy -map 0:v -map 0:a  -hls_time 10 -hls_list_size 6 -hls_flags delete_segments -hls_segment_filename $path/$stream->id/$stream->id_%03d.ts $path/$stream->id/master.m3u8";
-        $pid = exec('nohup /home/user/bin/ffmpeg -re -hide_banner -y -hwaccel auto -vsync 0 -stream_loop -1 -i '.$stream->vid_stream.'?fifo_size=1000000 '.$cmd.' </dev/null >/dev/null 2>/var/log/ffmpeg-'. $stream->id .'.log &', $op);
+        exec('nohup /home/user/bin/ffmpeg -re -hide_banner -y -hwaccel auto -vsync 0 -stream_loop -1 -i '.$stream->vid_stream.'?fifo_size=1000000 '.$cmd.' </dev/null >/dev/null 2>/var/log/ffmpeg-'. $stream->id .'.log  & echo $!', $op);
 
         $process = new JobProcess();
-        $process->pid = $pid;
+        $process->pid = $op[0];
         $process->name = $this->CHANNEL->name;
         $process->stream = $stream->vid_stream;
         $process->log =' var/log/ffmpeg-'. $stream->id .'.log';
