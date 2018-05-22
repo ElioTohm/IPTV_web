4eac0e4dedeca9cbeca24f2acaa52849310ff7e5
elio_tohme <eliotohme@gmail.com>
2018-05-22 18:26:32 +0300

    update

--- IPTV/app/Jobs/StreamPassThrough.php
+++ IPTV/app/Jobs/StreamPassThrough.php
@@ -51,7 +51,7 @@ class StreamPassThrough implements ShouldQueue
         $cmd = " -ignore_unknown -codec copy -map 0:v -map 0:a -hls_allow_cache 0  -hls_list_size $hls_list_size -hls_list_size $hls_list_size -hls_flags delete_segments -hls_segment_filename $path/$stream->id/$stream->id_%03d.ts $path/$stream->id/master.m3u8";
 
         // execute command as no hop and add echo $! to get pid of process
-        exec("/home/".env('USER')."/bin/ffmpeg $header_cmd  -i \"$stream->vid_stream?overrun_nonfatal=1&fifo_size=100000\" $cmd", $op);
+        exec("/home/".env('USER')."/bin/ffmpeg $header_cmd  -i \"$stream->vid_stream?overrun_nonfatal=1&fifo_size=200000\" $cmd  $tail_cmd", $op);
         
         // save information of the process
         $process = new JobProcess();
