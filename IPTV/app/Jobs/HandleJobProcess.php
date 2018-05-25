0052fe0697b458dbea50b14cc410f35c571f9296
elio_tohme <eliotohme@gmail.com>
2018-05-25 09:12:05 +0300

    fix mistake

--- IPTV/app/Jobs/HandleJobProcess.php
+++ IPTV/app/Jobs/HandleJobProcess.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\JobProcess;
+use App\JobProcess;
 
 class HandleJobProcess implements ShouldQueue
 {
