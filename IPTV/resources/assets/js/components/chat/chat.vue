f7a017e6b36ca6e603ea8ec51e73c2902d4df3e7
elio_tohme <eliotohme@gmail.com>
2018-06-25 11:14:18 +0300

    design fix

--- IPTV/resources/assets/js/components/chat/chat.vue
+++ IPTV/resources/assets/js/components/chat/chat.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="ChatBox">
         <div class="col-md-12 ChatBox__Right">
-            <chat-message v-for="message in messages" :data="message" :key="message.timestamp" :messagetype="message.type"></chat-message>
-            <input @keyup.enter="sendMessage" type="text" v-model="message" placeholder="Enter your message here">
+            <div class="inner_chatbox">
+                <chat-message class="ChatBox_messages" v-for="message in messages" :data="message" :key="message.timestamp" :messagetype="message.type"></chat-message>
+            </div>
+            <input class="Chat_input" @keyup.enter="sendMessage" type="text" v-model="message" placeholder="Enter your message here">
         </div>
     </div>
 </template>
@@ -31,12 +33,12 @@
         methods: {
             sendMessage(event) {
                 this.room = 'admin'
-                this.timestamp = new Date().getTime()
+                this.timestamp = new Date().toLocaleString() 
                 this.messages.push({ message: this.message, room: this.room, timestamp: this.timestamp})
                 var message =  this.message.split(":")
                 console.log(message)
                 if (message.length > 1) {
-                    var room = message[0].split(".")
+                    var room = message[0].split("@")
                     window.io
                     .emit("Notification_", {
                             room: room[1],
@@ -70,8 +72,19 @@
         margin: 0;
         padding: 0;
     }
+    .Chat_input {
+        position: absolute;
+        bottom: 0px;
+    }
+    .inner_chatbox {
+        height: 80vh;
+        max-height: 80vh;
+        overflow-y: scroll;
+    }
     .ChatBox__Right {
         height: 90vh;
+        max-height: 90vh;
+        position: relative;
         border-left: 1px solid #eee;
         background: #F7F7F7;
         box-shadow: -10px 0 40px #F1F1F1;
